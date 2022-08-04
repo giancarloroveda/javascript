37e89be490e95dd8e112cf2fdc99e8c4d87d1098
Giancarlo Roveda <108986946+giancarloroveda@users.noreply.github.com>
2022-08-04 17:50:52 -0300

    Create objetos.js

---
+++ Objetos/objetos.js
@@ -0,0 +1,20 @@
+const obj1 = {
+    nome: "Maria"
+}
+
+const obj2 = new Object()
+obj2.nome = "João"
+obj2['idade'] = 17
+
+console.log(obj1)
+console.log(obj2)
+
+const str1 = "minha string"
+
+console.log(str1.length)
+
+const str2 = new String("objeto do tipo string")
+
+console.log(str2)
+console.log(str2.length)
+console.log(str2.valueOf())
