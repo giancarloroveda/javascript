ebb2049f4f9875a42aab6a5627a64cf975b97580
Giancarlo Roveda <108986946+giancarloroveda@users.noreply.github.com>
2022-09-14 08:49:28 -0300

    Create constructors.js

---
+++ Objetos/constructors.js
@@ -0,0 +1,25 @@
+function Task(name) {
+    "use strict"
+    console.log(this)
+    let _name = name
+    this.createdAt = new Date()
+    this.updatedAt = null
+    this.getName = function () {
+        return _name
+    }
+    this.changeName = function (newName) {
+        if(newName) {
+            _name = newName
+            this.updatedAt = new Date()
+        }
+    }
+    console.log(this)
+}
+
+// const task1 = new Task('task 1')
+// console.log(task1.getName())
+// console.log(task1)
+// task1.changeName('TASK 1')
+// console.log(task1.getName())
+
+const task2 = new Task('task 2')
