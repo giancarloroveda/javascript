9a314ccc849234330c8d18c5f2d74ec0bbe9c552
Giancarlo Roveda <108986946+giancarloroveda@users.noreply.github.com>
2022-09-29 18:16:25 -0300

    date

---
+++ Date/datas_para_string.js
@@ -0,0 +1,8 @@
+const data = new Date()
+console.log(data)
+console.log(data.toString())
+console.log(data.toDateString())
+console.log(data.toISOString())
+console.log(data.toLocaleDateString())
+console.log(data.toLocaleString('pt-BR', {month: 'long', weekday: 'long', day: 'numeric', year: 'numeric', hour12: 'false', hour:'2-digit', minute: '2-digit', second: '2-digit'}))
+console.log(data.toUTCString())
---
+++ Date/introducao.js
@@ -0,0 +1,11 @@
+const data = new Date()
+console.log(data)
+console.log(data.getDay()) //dia da semana -> domingo = 0; sábado = 6
+console.log(data.getDate()) // dia do mês
+console.log(data.getMonth()) // mês do ano -> janeiro = 0; dezembro = 11
+console.log(data.getTime()) // número de milissegundos decorridos desde 1 de janeiro de 1970 à meia noite.
+console.log(data.getFullYear()) // ano
+console.log(data.getHours()) // hora
+console.log(data.getUTCHours()) // hora no UTC 0
+console.log(data.getMinutes()) //minutos (tem pra segundos e milisegundos e todos tem a versao em UTC)
+console.log(new Date(1969, 4, 11, 6, 30, 20, 8)) // ano, mês, dia, hora, minuto, segundo, milissegundo
